e67bf931aff29d0819fb717e0e7dda2dc40e3e96
uswatunbukanusus <namakuuswabukanusus@gmail.com>
2024-09-24 15:05:41 +0700

    perubahan

--- resources/views/pelanggan/layout/index.blade.php
+++ resources/views/pelanggan/layout/index.blade.php
@@ -10,54 +10,21 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="{{assets('css/customer.css'}}">
     <title>Toko online | {{$title}}</title>
-
+   
 </head>
 <body> 
     <main>
-    <nav class="navbar navbar-dark navbar-expand-lg" style="background-color: #00224D ">
-  <div class="container">
-    <a class="navbar-brand" href="#">TOWEBUS</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse justify-content-end gap-4" id="navbarSupportedContent">
-      <ul class="navbar-nav gap-4 ">
-        <li class="nav-item">
-          <a class="nav-link fs-5 active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link fs-5" href="#">Shop</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link fs-5" href="#">Contact Us</a>
-        </li>
-        <!-- <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li> -->
-        <!-- <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li> -->
-      </ul>
-      <div class="d-flex gap-4 align-items-center">
-        <!-- <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"> -->
-        <button class="btn btn-success" type="button">Login | Register</button>
-        <div class="notif">
-            <a href="#" class="fs- 5 ">
-              <i class="fa-solid icon-nav fa-bag-shopping"></i> 
-            </a>
-            <div class="circle">10</div>
-        </div>
-    </div>
-  </div>
-</nav>
+        <header>
+          @include('pelanggan.component.navbar')
+        </header>
+        <selection>
+          ini hsdfhsfh
+        </selection>
+        <footer>
+          <div class="container">
+            .d-flex
+          </div>
+        </footer>
     </main>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
