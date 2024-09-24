a73dd08b87965fbaf9e2464d25ff3151c47c8b40
uswatunbukanusus <namakuuswabukanusus@gmail.com>
2024-09-24 14:28:17 +0700

    tambahan

--- resources/views/pelanggan/layout/index.blade.php
+++ resources/views/pelanggan/layout/index.blade.php
@@ -8,14 +8,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <style>
-        .icon-nav{
-            color: white;
-        }
-        .icon-nav:hover{
-            color:white;
-        }
-    </style>
+    <link rel="stylesheet" href="{{assets('css/customer.css'}}">
     <title>Toko online | {{$title}}</title>
 
 </head>
@@ -56,9 +49,12 @@
       <div class="d-flex gap-4 align-items-center">
         <!-- <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"> -->
         <button class="btn btn-success" type="button">Login | Register</button>
-        <a href="#" class="fs- 5 ">
-            <i class="fa-solid icon-nav fa-bag-shopping"></i> 
-        </a>
+        <div class="notif">
+            <a href="#" class="fs- 5 ">
+              <i class="fa-solid icon-nav fa-bag-shopping"></i> 
+            </a>
+            <div class="circle">10</div>
+        </div>
     </div>
   </div>
 </nav>
